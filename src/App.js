6194551fb046f1463a7447cb618d47f7b6be4b79
ABojo <alex.bojorquez2598@gmail.com>
2021-06-26 18:03:32 -0400

    Created a settings state objec to control what kind of password is generated/ wrote a function that will build a string of available characters to randomly choose from based on the settings object/wrote a function that will generate a new password using the string of allowed chars

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import PasswordBox from './components/PasswordBox';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const lowerString = 'abcdefghijklmnopqrstuvwxyz';
+  const upperString = lowerString.toUpperCase();
+  const numsString = '0123456789';
+  const symbolsString = '!@#$%^&*';
+
+  const [settings, setSettings] = useState({
+    length: 12,
+    uppercase: true,
+    numbers: true,
+    symbols: true,
+  });
+  const [password, setPassword] = useState('');
+
+  //Uses short circuting to build the string that random characters will be chosen from
+  const buildCharsString = () => {
+    const { uppercase, numbers, symbols } = settings;
+
+    return `${lowerString}${uppercase && upperString}${numbers && numsString}${
+      symbols && symbolsString
+    }`;
+  };
+
+  const generatePassword = () => {
+    const chars = buildCharsString();
+    let newPassword = '';
+
+    for (let i = 0; i < settings.length; i++) {
+      newPassword += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    setPassword(newPassword);
+  };
+
+  useEffect(() => {
+    generatePassword();
+  }, []);
+
   return (
     <div className="bg-true-gray-900 min-h-screen py-12">
       <div className="mx-auto max-w-4xl w-11/12">
-        <PasswordBox />
+        <PasswordBox password={password} generatePassword={generatePassword} />
       </div>
     </div>
   );
--- src/components/PasswordBox.js
+++ src/components/PasswordBox.js
@@ -1,15 +1,16 @@
-function PasswordBox() {
+function PasswordBox(props) {
+  const { password, generatePassword } = props;
   return (
     <div className="w-full bg-true-gray-700 rounded shadow relative">
       <input
-        value="^ZlmzFsUtXw6xqZdMlo0AAAAAAAAAAAAAA"
+        value={password}
         className="rounded p-5 pr-32 bg-true-gray-700 w-full text-true-gray-100 font-bold text-2xl"
       ></input>
       <div className="absolute top-1/2 right-5 text-blue-100 text-3xl transform -translate-y-1/2 ">
         <button>
           <i class="fas fa-copy mr-6 hover:text-blue-500 transition duration-200"></i>
         </button>
-        <button>
+        <button onClick={generatePassword}>
           <i class="fas fa-random hover:text-blue-500 transition duration-200"></i>
         </button>
       </div>
