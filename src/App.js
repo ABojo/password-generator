c3fcbf9e57649f6761fdb88be3582f031d12d719
ABojo <alex.bojorquez2598@gmail.com>
2021-06-26 16:29:50 -0400

    created a password box component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import PasswordBox from './components/PasswordBox';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="bg-true-gray-900 min-h-screen py-12">
+      <div className="mx-auto max-w-4xl w-11/12">
+        <PasswordBox />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PasswordBox.js
@@ -0,0 +1,20 @@
+function PasswordBox() {
+  return (
+    <div className="w-full bg-true-gray-700 rounded shadow relative">
+      <input
+        value="^ZlmzFsUtXw6xqZdMlo0AAAAAAAAAAAAAA"
+        className="rounded p-5 pr-32 bg-true-gray-700 w-full text-true-gray-100 font-bold text-2xl"
+      ></input>
+      <div className="absolute top-1/2 right-5 text-blue-100 text-3xl transform -translate-y-1/2 ">
+        <button>
+          <i class="fas fa-copy mr-6 hover:text-blue-500 transition duration-200"></i>
+        </button>
+        <button>
+          <i class="fas fa-random hover:text-blue-500 transition duration-200"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PasswordBox;
