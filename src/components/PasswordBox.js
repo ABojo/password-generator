4d8e8a3afdf0d94fb06a05fa7a7ce790e764367c
ABojo <alex.bojorquez2598@gmail.com>
2021-06-26 21:29:54 -0400

    more styling updates

--- src/components/PasswordBox.js
+++ src/components/PasswordBox.js
@@ -4,9 +4,9 @@ function PasswordBox(props) {
     <div className="w-full mb-8 flex items-stretch">
       <input
         value={password}
-        className="rounded p-8 bg-true-gray-700 w-full text-white font-bold text-xl mr-3 "
+        className="rounded p-5 bg-true-gray-700 w-full text-white text-xl mr-3 "
       ></input>
-      <button className="bg-blue-500 p-5 text-white rounded hover:bg-blue-600  transition duration-200">
+      <button className="bg-blue-500 p-5 text-white rounded hover:bg-blue-600 transition duration-200">
         <i class="fas fa-copy text-white text-3xl"></i>
       </button>
     </div>
